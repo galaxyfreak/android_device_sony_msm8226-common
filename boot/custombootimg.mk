9cd7348d78239bbc20c3eee789b4ef3b6b2f77ae
Adam Farden <adam@farden.cz>
2015-05-02 16:53:40 +0200

    Use a standard boot image
    We've grown used to egregious hacks this century. It's hard to
    dazzle us. But for nearly 5 years custombootimg.mk has been doing
    just that. We've grown used to the idea of packing recovery into
    the boot image, and perhaps we forget that we've been doing it wrong.
    
    We're still pioneers. We're still finding better ways of doing
    things. And now with real boot loaders for Open Devices, it's
    time to say farewell to an old friend. The future doesn't belong
    to unsustainable hacks; it belongs to clean, open code. These new
    boot loaders are pulling us into the future, and we'll continue
    to follow them.
    
    Change-Id: I6fece1831062967c2d3ae930d472baa8d9b4979e
    Signed-off-by: Adam Farden <adam@farden.cz>

--- boot/custombootimg.mk
+++ boot/custombootimg.mk
@@ -1,12 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-uncompressed_ramdisk := $(PRODUCT_OUT)/ramdisk.cpio
-$(uncompressed_ramdisk): $(INSTALLED_RAMDISK_TARGET)
-	zcat $< > $@
-
-INITSH := device/sony/msm8226-common/boot/init.sh
-BOOTREC_DEVICE := device/sony/$(TARGET_DEVICE)/bootrec-device
-
 ## Build and run dtbtool
 DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolCM$(HOST_EXECUTABLE_SUFFIX)
 INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
@@ -19,29 +12,9 @@ $(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/u
 
 
 ## Overload bootimg generation: Same as the original, + --dt arg
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET) \
-    $(INSTALLED_KERNEL_TARGET) $(uncompressed_ramdisk) $(recovery_uncompressed_ramdisk) \
-    $(INITSH) $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/utilities/extract_elf_ramdisk
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
 	$(call pretty,"Target boot image: $@")
-
-	$(hide) rm -fr $(PRODUCT_OUT)/combinedroot
-	$(hide) mkdir -p $(PRODUCT_OUT)/combinedroot/sbin
-
-	$(hide) cp $(PRODUCT_OUT)/root/logo.rle $(PRODUCT_OUT)/combinedroot/logo.rle
-	$(hide) cp $(uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
-	$(hide) cp $(recovery_uncompressed_ramdisk) $(PRODUCT_OUT)/combinedroot/sbin/
-	$(hide) cp $(PRODUCT_OUT)/utilities/busybox $(PRODUCT_OUT)/combinedroot/sbin/
-	$(hide) cp $(PRODUCT_OUT)/utilities/extract_elf_ramdisk $(PRODUCT_OUT)/combinedroot/sbin/
-
-	$(hide) cp $(INITSH) $(PRODUCT_OUT)/combinedroot/sbin/init.sh
-	$(hide) chmod 755 $(PRODUCT_OUT)/combinedroot/sbin/init.sh
-	$(hide) ln -s sbin/init.sh $(PRODUCT_OUT)/combinedroot/init
-	$(hide) cp $(BOOTREC_DEVICE) $(PRODUCT_OUT)/combinedroot/sbin/
-
-	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
-	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
-
-	$(hide) $(MKBOOTIMG) --kernel $(INSTALLED_KERNEL_TARGET) --ramdisk $(PRODUCT_OUT)/combinedroot.fs --cmdline "$(BOARD_KERNEL_CMDLINE)" --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --dt $(INSTALLED_DTIMAGE_TARGET) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
